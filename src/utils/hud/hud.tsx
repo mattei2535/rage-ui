15adb0bd827969488e55b4382acdfaa67d91c274
matei <mateibratu14@gmail.com>
2023-02-19 13:43:17 +0200

    Started ingame HUD

--- src/utils/hud/hud.tsx
+++ src/utils/hud/hud.tsx
@@ -3,11 +3,22 @@ import '../../index.css';
 const Hud = () => {
 
     return (
+        
         <div>
-            <h1 className="font-serif font-bold text-center text-white mt-2">WORK IN PROGRESS</h1>
-            <div className='absolute'>
-                <p id='user'>Player(ID)</p>
+            
+            <img src='/images/hudlogo2.png/' className='relative float-right mr-[30px] mt-[40px]'/>
+            <div className='relative flex space-x-5 float-right mr-4 mt-[10px] text-xl'>
+                <h1>ID: 0</h1>
+                <h1>|</h1>
+                <h1>mtw</h1>
             </div>
+            <div className='relative'>
+                <img src='/images/clock.png/' className='float-right mt-[10px] ml-[100px] mr-[-320px]'/>
+                <img src='/images/date.png/' className='float-right mt-[10px] mr-[-200px]'/>
+            </div>
+
+            
+            
         </div>
     )
 }
