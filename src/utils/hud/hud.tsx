8cf6979af052037e65b50cecdc134282a48ff5f1
mattei <matei2535@gmail.com>
2023-03-04 16:31:52 +0200

    Added 'money' in the hud UI

--- src/utils/hud/hud.tsx
+++ src/utils/hud/hud.tsx
@@ -3,9 +3,7 @@ import '../../index.css';
 const Hud = () => {
 
     return (
-        
         <div>
-            
             <img src='/images/hudlogo2.png/' className='relative float-right mr-[30px] mt-[40px]'/>
             <div className='relative flex space-x-5 float-right mr-4 mt-[10px] text-xl'>
                 <h1>ID: 0</h1>
@@ -15,9 +13,12 @@ const Hud = () => {
             <div className='relative'>
                 <img src='/images/clock.png/' className='float-right mt-[10px] ml-[100px] mr-[-320px]'/>
                 <img src='/images/date.png/' className='float-right mt-[10px] mr-[-200px]'/>
+                <img src='/images/dollar.png' className='relative float-right w-[35px] h-[35px] mt-[260px] mr-[-380px]'/>
+                <h1 className='relative text-white text-2xl float-right mt-[260px] mr-[-330px] align-middle font-Oswald'>1312332312</h1>
+
+
             </div>
 
-            
             
         </div>
     )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Oswald: ['Oswald', 'sans-serif'],
+      }
+    },
     },
   plugins: [],
 }
